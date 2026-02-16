c8f8db17e0b7f46be79861586d2f63d62c8b3387
SANVI <sanvijsr1@gmail.com>
2026-02-16 19:39:07 +0530

    weather app

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -16,7 +16,25 @@ const Weather = () => {
             <input type="text" placeholder='Search' />
             <img src={search_icon} alt="" />
         </div>
-        <img src={clear_icon} alt="" />
+        <img src={clear_icon} alt="" className='weather-icon'/>
+        <p className='temperature'>16°c</p>
+        <p className='location'>London</p>
+        <div className='weather-data'>
+          <div className="col">
+            <img src={humidity_icon} alt="" />
+            <div>
+              <p>91%</p>
+              <span>Humidity</span>
+            </div>
+          </div>
+          <div className="col">
+            <img src={wind_icon} alt="" />
+            <div>
+              <p>3.6 Km/h</p>
+              <span>Wind speed</span>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
